a0069a617990b002df5b5de08070afac6eaa14ad
EliterScripts <eli@mrwebb.net>
2018-12-12 11:25:47 -0700

    added https server

---
+++ templates/https.lua
@@ -0,0 +1,11 @@
+do
+    return {
+        domain_name,
+        admin_email,
+        domain_name,
+        domain_name,
+        domain_name,
+        domain_name,
+        domain_name,
+    }
+end
