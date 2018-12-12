bd94962863bc618b751a65e0ac442fe6c2290426
EliterScripts <eli@mrwebb.net>
2018-12-12 11:29:37 -0700

    added secure site

--- index.lua
+++ index.lua
@@ -51,4 +51,36 @@ do
     commands.reloadApache()
 
     commands.addLetsEncrypt(domain_name)
+    
+    
+    
+    
+    file = io.open("templates/https.conf", "r")
+    local https_template_string = file:read("*all")
+    file:close()
+    local https_template_map = require("templates/https")
+
+    local https_output_string = string.format(
+        https_template_string, 
+
+        unpack(
+            https_template_map
+        )
+    )
+
+    addHistory({
+        domain_name = domain_name,
+        admin_email = admin_email,
+        annotation = "simply creating a website that is simple. Creating HTTPS site.",
+        action_code = 1001
+    })
+
+    --writes the https site
+    file = io.open("/etc/apache2/sites-available/regular-" .. domain_name .. "-secure.conf", "w")
+    file:write(https_output_string)
+    file:close()
+    
+    commands.enableSite("regular-" .. domain_name .. "-secure.conf")
+
+    commands.reloadApache()
 end
